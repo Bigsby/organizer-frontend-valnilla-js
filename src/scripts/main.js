8c52c75001f409f5e2a97371c64250c242c58b9c
Bruno Bernardo <bigsby@live.com>
2018-12-10 19:53:56 +0000

    navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,65 @@
-function handleNavigation(hash) {
-    console.log("new hash: " + hash);
+const routes = {
+    configs: [
+        {
+            path: "/",
+            template: "home"
+        },
+        {
+            path: "/contacts",
+            template: "contacts"
+        },
+        {
+            path: "/calendar",
+            template: "calendar"
+        }
+    ],
+    default: "/",
+    getDefault: function() {
+        const thisDefault = this.default;
+        return this.configs.find(c => c.path === thisDefault);
+    },
+    getRoute: function(route){
+        let foundConfig = this.getDefault();
+        this.configs.forEach(c => {
+            if (route.startsWith(c.path)) {
+                foundConfig = c;
+            }
+        });
+        return foundConfig;
+    }
+};
+let mainContainer;
+const templateCache = {};
+
+function getTemplate(template, callback) {
+    const cachedTemplate = templateCache[template];
+    if (cachedTemplate) {
+        callback(cachedTemplate);
+    } else {
+        fetch("templates/" + template + ".html").then(response => {
+            response.text().then(textResponse => callback(templateCache[template] = textResponse));
+        });
+    }
 }
 
-function setWindowListeners() {
+function handleNavigation(hash) {
+    const config = routes.getRoute(hash.substring(1));
+    getTemplate(config.template, template => {
+        mainContainer.innerHTML = template;
+    });
+    // mainContainer.innerText = config.template;
+}
+
+function initialize() {
     window.addEventListener("hashchange", (ev) => {
         ev.preventDefault();
         ev.returnValue = "";
         handleNavigation(window.location.hash);
     }, true);
     window.addEventListener("load", () => {
-        
+        mainContainer = document.querySelector("#mainContainer");
+        handleNavigation(window.location.hash);
     });
 }
 
-setWindowListeners();
+initialize();
